20f5a690fb2082b6ba8496edae11834b652b5154
Vadim Laletin <ruscoder@users.noreply.github.com>
2024-12-02 19:51:16 +0100

    Make aws settings not required

--- app/config.py
+++ app/config.py
@@ -5,7 +5,8 @@ is_test_environment = os.environ.get("TEST_ENV", False)
 aws_bucket = os.environ.get("AWS_BUCKET", "dataintake")
 bucket_prefix = os.environ.get("BUCKET_PREFIX", "aiobotocore")
 link_ttl = int(os.environ.get("LINK_TTL", "600"))
-region_name = os.environ["REGION_NAME"]
-aws_access_key_id = os.environ["AWS_ACCESS_KEY_ID"]
-aws_secret_access_key = os.environ["AWS_SECRET_ACCESS_KEY"]
-minio_endpoint = os.environ["MINIO_ENDPOINT"]
+
+region_name = os.environ.get("REGION_NAME")
+aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
+aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
+minio_endpoint = os.environ.get("MINIO_ENDPOINT")
